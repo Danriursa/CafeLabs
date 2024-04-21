cb64f8c8fdd37db76986caef36a0a2d0f659ce80
Daniel Ricardo Urbina Sandoval <113052999+Danriursa@users.noreply.github.com>
2024-04-21 10:59:29 -0500

    Req: Actualización de variables de entorno

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import productosRoutes from "./routes/productosRoutes.js";
 import pedidoRoutes from "./routes/pedidoRoutes.js";
 import inventarioRoutes from "./routes/inventarioRoutes.js";
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 //Crear el servidor
 const app= express();
@@ -16,8 +19,8 @@ app.use('/pedido',pedidoRoutes);
 app.use('/inventario',inventarioRoutes);
 
 
-const direccion = 'localhost:27017';
-const db = 'CafeLab'
+const direccion = process.env.direccion;
+const db = process.env.db;
 
 
 mongoose.connect(`mongodb://${direccion}/${db}`,{
@@ -29,7 +32,7 @@ mongoose.connect(`mongodb://${direccion}/${db}`,{
 
 
 //Puerto
-const port = 4000;
+const port = process.env.port;
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`)
 });
